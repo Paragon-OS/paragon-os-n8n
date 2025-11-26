a34a7504a20419b0ec101e7366dda36eb1ef15d1
ParagonTheDev <nipuna.sudha@gmail.com>
2025-11-26 09:14:31 +0530

    Update ESLint configuration to disable specific rules for improved development flexibility
    - Disabled console logging, unused variable checks, explicit any type warnings, and exhaustive dependencies checks in React hooks.
    - This adjustment aims to enhance the development experience by allowing more flexibility during the coding process.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    "no-console": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "react-hooks/exhaustive-deps": "off",
+  },
 ];
 
 export default eslintConfig;
